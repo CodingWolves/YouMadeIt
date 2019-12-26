c9459e1d1e008dbcbad4c510b4ed45e79889a469
Nadav Weisler <32937058+nadavWeisler@users.noreply.github.com>
2019-12-26 15:03:17 +0200

    Update app.py
    just look nice

--- app.py
+++ app.py
@@ -7,8 +7,7 @@ from flask import Flask, request, send_from_directory
 import hashlib
 
 import firebase_admin
-from firebase_admin import credentials, auth, exceptions
-from firebase_admin import firestore
+from firebase_admin import firestore, credentials
 
 cred = credentials.Certificate("config.json")
 firebase_admin.initialize_app(cred)
@@ -32,14 +31,14 @@ def start():
 @app.route('/sessionLogout', methods=['POST'])
 def session_logout():
     session_cookie = flask.request.cookies.get('session')
-    try:
-        decoded_claims = auth.verify_session_cookie(session_cookie)
-        auth.revoke_refresh_tokens(decoded_claims['sub'])
-        response = flask.make_response(flask.redirect('/login'))
-        response.set_cookie('session', expires=0)
-        return response
-    except auth.InvalidSessionCookieError:
-        return flask.redirect('/login')
+    # try:
+    #     decoded_claims = auth.verify_session_cookie(session_cookie)
+    #     auth.revoke_refresh_tokens(decoded_claims['sub'])
+    #     response = flask.make_response(flask.redirect('/login'))
+    #     response.set_cookie('session', expires=0)
+    #     return response
+    # except auth.InvalidSessionCookieError:
+    #     return flask.redirect('/login')
 
 
 @app.route('/profile', methods=['POST'])
@@ -61,18 +60,18 @@ def login():
     id_token = flask.request.json['idToken']
     # Set session expiration to 5 days.
     expires_in = datetime.timedelta(days=5)
-    try:
-        # Create the session cookie. This will also verify the ID token in the process.
-        # The session cookie will have the same claims as the ID token.
-        session_cookie = auth.create_session_cookie(id_token, expires_in=expires_in)
-        response = flask.jsonify({'status': 'success'})
-        # Set cookie policy for session cookie.
-        expires = datetime.datetime.now() + expires_in
-        response.set_cookie(
-            'session', session_cookie, expires=expires, httponly=True, secure=True)
-        return response
-    except exceptions.FirebaseError:
-        return flask.abort(401, 'Failed to create a session cookie')
+    # try:
+    #     # Create the session cookie. This will also verify the ID token in the process.
+    #     # The session cookie will have the same claims as the ID token.
+    #     session_cookie = auth.create_session_cookie(id_token, expires_in=expires_in)
+    #     response = flask.jsonify({'status': 'success'})
+    #     # Set cookie policy for session cookie.
+    #     expires = datetime.datetime.now() + expires_in
+    #     response.set_cookie(
+    #         'session', session_cookie, expires=expires, httponly=True, secure=True)
+    #     return response
+    # except exceptions.FirebaseError:
+    #     return flask.abort(401, 'Failed to create a session cookie')
 
 
 def user_exist_from_values(values):
